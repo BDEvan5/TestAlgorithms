3a2e361796de16c1aad72f7f77f1c0bff30cc222
Benjamin <benjaminevans316@gmail.com>
2020-09-04 17:23:39 +0000

    More tries

--- TrajOpt/ShortestDistOCP.py
+++ TrajOpt/ShortestDistOCP.py
@@ -16,8 +16,6 @@ cs = track[:, 1] / (track[:, 0] * ms)
 track[:, 2] = ms 
 track[:, 3] = cs
 
-# calculate nvecs and add to dim, 4, 5
-
 l = 0.33
 a_max = 7.5
 d_max = 0.4
@@ -43,60 +41,65 @@ c1_f = MX.sym('c1', N-1)
 m0_f = MX.sym('m0', N-1)
 m1_f = MX.sym('m1', N-1)
 
-inter_x = Function('inter', [c0_f, m0_f], [(track[1:, 3] - c0_f)/(m0_f - track[1:, 2])])
-inter_y = Function('inter', [c0_f, m0_f], 
-                [(c0_f*track[1:, 2] - track[1:, 3]*m0_f) / (track[1:, 2] - m0_f)])
-
-get_c = Function('get_c', [m0_f, x_f, y_f], [y_f / (m0_f * x0_f)])
-
-get_nx = Function('nx', [n_f, th_f], [inter_x(get_c(tan(th_f), track[:-1, 0], track[:-1, 1]), tan(th_f))])
-get_ny = Function('ny', [n_f, th_f], [inter_y(get_c(tan(th_f), track[:-1, 0], track[:-1, 1]), tan(th_f))])
-
-dis = Function('dis', [x0_f, x1_f, y0_f, y1_f], [sqrt((x1_f-x0_f)**2 + (y1_f-y0_f)**2)])
-d_n = Function('d_n', [n_f, th_f], [dis(get_nx(n_f, th_f), track[1:, 0], get_ny(n_f, th_f), track[1:, 1])])
-
-d_th = Function('d_th', [v_f, d_f], [v_f/l * tan(d_f)])
-
 o_x_s = Function('o_x', [n_f], [track[:-1, 0] + nvecs[:-1, 0] * n_f])
 o_y_s = Function('o_y', [n_f], [track[:-1, 1] + nvecs[:-1, 1] * n_f])
 o_x_e = Function('o_x', [n_f], [track[1:, 0] + nvecs[1:, 0] * n_f])
 o_y_e = Function('o_y', [n_f], [track[1:, 1] + nvecs[1:, 1] * n_f])
 
+get_c = Function('get_c', [m0_f, x_f, y_f], [y_f / (m0_f * x0_f)])
+dis = Function('dis', [x0_f, x1_f, y0_f, y1_f], [sqrt((x1_f-x0_f)**2 + (y1_f-y0_f)**2)])
+
+inter_x = Function('inter', [c0_f, m0_f], [(cs[1:] - c0_f)/(m0_f - ms[1:])])
+inter_y = Function('inter', [c0_f, m0_f], [(c0_f*ms[1:] - cs[1:]*m0_f) / (ms[1:] - m0_f)])
+
+get_nx = Function('nx', [n_f, th_f], [inter_x(get_c(tan(th_f), o_x_s(n_f), o_y_s(n_f)), tan(th_f))])
+get_ny = Function('ny', [n_f, th_f], [inter_y(get_c(tan(th_f), o_x_s(n_f), o_y_s(n_f)), tan(th_f))])
+
+
+d_n = Function('d_n', [n_f, th_f], [dis(get_nx(n_f, th_f), track[1:, 0], get_ny(n_f, th_f), track[1:, 1])])
+
 track_length = Function('length', [n_f_a], [dis(o_x_s(n_f_a[:-1]), o_x_e(n_f_a[1:]), 
                             o_y_s(n_f_a[:-1]), o_y_e(n_f_a[1:]))])
 
-# d_x = Function('d_x', [n_f, v_f, th_f, t_f], [o_x_s(n_f) + v_f * sin(th_f) * (t_f[1:] - t_f[:-1])])
-# d_y = Function('d_y', [n_f, v_f, th_f, t_f], [o_y_s(n_f) + v_f * cos(th_f) ])
+# d_th = Function('d_th', [v_f, d_f], [v_f/l * tan(d_f)])
 
-# d_n = Function('d_n', [n_f, th_f], [dis(d_x(n_f, th_f), o_x_e(n_f), \
-                                                # d_y(n_f, th_f), o_y_e(n_f))])
 
 
 # define symbols
 n = MX.sym('n', N)
 # t = MX.sym('t', N)
 # v = MX.sym('v', N)
-# th = MX.sym('th', N)
+th = MX.sym('th', N)
 # a = MX.sym('a', N)
 # d = MX.sym('d', N)
 
-vs = np.ones(N) * 2
-
-nlp_g = vertcat(
+nlp = {\
+    'x': vertcat(n, th),
+    'f': sumsqr(track_length(n)),
+    'g': vertcat(
                 # dynamic constraints
                 # th[1:] - (th[:-1] + d_th(vs[:-1], d[:-1])),
-                # n[1:] - (d_n(n[:-1], th[:-1])),
+                n[1:] - (d_n(n[:-1], th[:-1])),
 
                 # boundary constraints
-                n[0], 
-                n[-1],
-            ) 
-
-nlp = {\
-    'x': vertcat(n),
-    'f': sumsqr(track_length(n)),
-    'g':  nlp_g\
+                n[0], th[0],
+                n[-1], th[-1]
+            ) \
     }
+    
+# nlp = {\
+#     'x': vertcat(n),
+#     'f': sumsqr(track_length(n)),
+#     'g': vertcat(
+#                 # dynamic constraints
+#                 # th[1:] - (th[:-1] + d_th(vs[:-1], d[:-1])),
+#                 # n[1:] - (d_n(n[:-1], th[:-1])),
+
+#                 # boundary constraints
+#                 n[0], 
+#                 n[-1]
+#             ) \
+#     }
 
 S = nlpsol('S', 'ipopt', nlp)
 
@@ -114,10 +117,11 @@ th0 = np.array(0)
 # d0 = atan(l * (th0[1:] - th0[:-1]) / vs[:-1])
 # d0 = np.insert(np.array(d0), -1, 0)
 
-x0 = vertcat(n0)
+x0 = vertcat(n0, th0)
+# x0 = vertcat(n0)
 
-lbx = [-n_max]*N #+ [-np.pi]*N + [-d_max]*N
-ubx = [n_max]*N #+[np.pi]*N + [d_max]*N
+lbx = [-n_max]*N + [-np.pi]*N + [-d_max]*N
+ubx = [n_max]*N +[np.pi]*N + [d_max]*N
 
 r = S(x0=x0, lbg=0, ubg=0, lbx=lbx, ubx=ubx)
 
